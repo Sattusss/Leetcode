37deb933a8ac0f2e804285f3447f41fbfa775a42
SATYAM TIWARI <itssatyam.345@gmail.com>
2023-07-19 12:49:00 +0530

    New

---
+++ 435. Non-overlapping Intervals.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        if len(intervals) <= 1: return 0
+        intervals.sort(key=lambda x: x[1])
+        count = 0
+        end = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < end:
+                count += 1
+            else:
+                end = intervals[i][1]
+        return count
+    
