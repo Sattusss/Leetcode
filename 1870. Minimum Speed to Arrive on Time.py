1209690db747a37c3abbdf8184664f76d71a762b
SATYAM TIWARI <itssatyam.345@gmail.com>
2023-07-26 18:17:42 +0530

    Solved error

--- 1870. Minimum Speed to Arrive on Time.py
+++ 1870. Minimum Speed to Arrive on Time.py
@@ -1,28 +1,21 @@
-from typing import List
-
 class Solution:
-    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-        if hour < len(dist) - 1:
-            return -1
-        
-        l, r = 1, 10 ** 7
-        
-        while l < r:
-            m = l + (r - l) // 2
-            
-            if self.canReach(dist, hour, m):
-                r = m
-            else:
-                l = m + 1
-        
-        return l
-    
-    def canReach(self, dist: List[int], hour: float, speed: int) -> bool:
-        total = 0
-        
-        for i in range(len(dist) - 1):
-            total += (dist[i] + speed - 1) // speed
-        
-        total += dist[-1] / speed
-        
-        return total <= hour
+  def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+    ans = -1
+    l = 1
+    r = int(1e7)
+
+    def time(speed: int) -> float:
+      summ = 0
+      for i in range(len(dist) - 1):
+        summ += math.ceil(dist[i] / speed)
+      return summ + dist[-1] / speed
+
+    while l <= r:
+      m = (l + r) // 2
+      if time(m) > hour:
+        l = m + 1
+      else:
+        ans = m
+        r = m - 1
+
+    return ans
